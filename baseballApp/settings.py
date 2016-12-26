e85efd0b82947868026babdcb043e2f9fb983009
bweick <brian@biometrixtech.com>
2016-12-26 11:21:10 -0700

    making heroku ready

--- baseballApp/settings.py
+++ baseballApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,6 +112,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
