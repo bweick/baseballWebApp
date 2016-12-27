e85d186afcf737aea56d9f495c10d23c0c77fd84
bweick <brian@biometrixtech.com>
2016-12-27 13:39:41 -0700

    fix repository

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\brian\docume~1\baseba~1\mlb\webapp\baseba~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
