c20022e8ed4c796717ee93cdba9caefe0758a14b
bweick <brian@biometrixtech.com>
2017-01-08 15:56:27 -0700

    changes config default

--- baseballApp/settings.py
+++ baseballApp/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
