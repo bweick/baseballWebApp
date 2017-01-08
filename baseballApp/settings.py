aedb30f3371ea61a98d7184dc1aa51267ea2d33b
bweick <brian@biometrixtech.com>
2017-01-08 15:57:49 -0700

    undo previous commit

--- baseballApp/settings.py
+++ baseballApp/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
