4c9abdc19a4a6c75efdf7e7a90208aed37a97a03
bweick <brian@biometrixtech.com>
2017-01-09 16:31:43 -0700

    changing home page to statview

--- baseballApp/urls.py
+++ baseballApp/urls.py
@@ -24,6 +24,6 @@ router.register(r'api', views.StatsViewSet)
 urlpatterns = [
     url(r'^statview/', include('statview.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
+    url(r'^', include('statview.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
